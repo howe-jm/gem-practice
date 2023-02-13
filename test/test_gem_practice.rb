827497dde0e51f5cd8bf318ba5a9d0737a16b431
Jason Howe <trisynth@gmail.com>
2023-02-13 17:38:05 -0500

    Implements tests of existing and future functionality.

--- test/test_gem_practice.rb
+++ test/test_gem_practice.rb
@@ -3,11 +3,19 @@
 require "test_helper"
 
 class TestGemPractice < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::GemPractice::VERSION
+  def test_that_it_returns_false_for_non_palindromes
+    refute "foo".palindrome?
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_that_it_returns_true_for_simple_palindromes
+    assert "foof".palindrome?
+  end
+
+  def test_that_it_returns_correctly_regardless_of_case
+    assert "FoOf".palindrome?
+  end
+
+  def test_that_it_ignores_punctuation_and_whitespace
+    assert "Madam, I'm Adam.".palindrome?
   end
 end
