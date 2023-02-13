1961f2ea1f533b034f1398110fb6410cb2018826
Jason Howe <trisynth@gmail.com>
2023-02-13 17:15:24 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+### Just a little practice, nothing to see here.
---
+++ lib/gem_practice.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "gem_practice/version"
+
+module GemPractice
+  class Error < StandardError; end
+  # Your code goes here...
+end
